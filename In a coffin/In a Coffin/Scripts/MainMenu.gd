95a23a293ac0421158b9fa3ee7c7efa91e599969
WalterScherk <44490381+WalterScherk@users.noreply.github.com>
2023-09-30 17:27:27 +0200

    Exit and start correction

--- In a coffin/In a Coffin/Scripts/MainMenu.gd
+++ In a coffin/In a Coffin/Scripts/MainMenu.gd
@@ -5,3 +5,8 @@ extends Node2D
 func _on_texture_button_2_pressed():
 	get_tree().change_scene_to_file("res://Scenes/Coffin.tscn")
 	print("clic")
+
+
+func _on_texture_button_pressed():
+	get_tree().quit()
+	print("destroy window")
