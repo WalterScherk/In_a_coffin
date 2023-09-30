2cedd6018700565e6dd04986d258341b3d8a278e
Pol Lleida Ramirez <pol.lleida@enti.cat>
2023-09-30 18:28:45 +0200

    SpawnText

--- In a coffin/In a Coffin/Scripts/GameManager.gd
+++ In a coffin/In a Coffin/Scripts/GameManager.gd
@@ -6,21 +6,20 @@ var punches
 var isInjured
 var cronometer
 var punchInput
+var spawnedThing
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	hope = 100
 	punches = 0
 	isInjured = false
-	$PoppingTextManager.SpawnText("Hola")
+	spawnedThing = $PoppingTextManager.SpawnText("Hola")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	
 	if Input.is_action_just_pressed("ui_select"):
 		punches += 1
 		print(punches)
 
 	if hope > 0:
 		hope -= 1 * delta
-	
--- In a coffin/In a Coffin/Scripts/PopingTextManager.gd
+++ In a coffin/In a Coffin/Scripts/PopingTextManager.gd
@@ -2,11 +2,11 @@ extends Node2D
 
 func SpawnText(text):
 	var rng = RandomNumberGenerator.new()
-	var xPos = rng.randf_range(60, 1000)
-	var yPos = rng.randf_range(60, 580)
 	var touchButton = TouchScreenButton.new()
 	var label = Label.new()
 	
+	var xPos = rng.randf_range(60, 1000)
+	var yPos = rng.randf_range(60, 580)
 	label.position = Vector2(xPos, yPos)
 	
 	
